172997a9a679feadde842cce39740a80449d94be
Lennie Wennerlund <lennie@lempa.se>
2021-07-18 00:11:20 +0200

    wip

--- LiTools.Helpers.Convert/Megabytes.cs
+++ LiTools.Helpers.Convert/Megabytes.cs
@@ -1,18 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace LiTools.Helpers.Convert
 {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Convert Megabytes into ??.
+    /// </summary>
     public static class Megabytes
     {
-        public static double ToGigabytes(double megabytes) 
+        /// <summary>
+        /// Convert to bytes.
+        /// </summary>
+        /// <param name="megabytes">megabytes.</param>
+        /// <returns>bytes as double.</returns>
+        public static double ToBytes(double megabytes)
+        {
+            return megabytes * 1024.0 * 1024.0;
+        }
+
+        /// <summary>
+        /// Convert to kilobytes.
+        /// </summary>
+        /// <param name="megabytes">megabytes.</param>
+        /// <returns>kilobytes as double.</returns>
+        public static double ToKiloBytes(double megabytes)
+        {
+            return megabytes * 1024.0;
+        }
+
+        /// <summary>
+        /// Convert megabytes to gigabytes.
+        /// </summary>
+        /// <param name="megabytes">megabytes.</param>
+        /// <returns>Gigabytes as double.</returns>
+
+        public static double ToGigabytes(double megabytes)
         {
             // 1024 megabyte in a gigabyte
             return megabytes / 1024.0;
         }
 
-        public static double ToTerabytes(double megabytes) 
+        /// <summary>
+        /// Convert megabytes to terabytes.
+        /// </summary>
+        /// <param name="megabytes">megabytes.</param>
+        /// <returns>Gigabytes as double.</returns>
+        public static double ToTerabytes(double megabytes)
         {
             // 1024 * 1024 megabytes in a terabyte
             return megabytes / (1024.0 * 1024.0);
