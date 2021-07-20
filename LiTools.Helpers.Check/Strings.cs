68bf20bb41e15dc973e3bdcb204a4215f0ad98d1
Lennie Wennerlund <lennie@lempa.se>
2021-07-20 13:24:35 +0200

    Created new project.

---
+++ LiTools.Helpers.Check/Strings.cs
@@ -0,0 +1,22 @@
+﻿// <summary>
+// Check string data.
+// </summary>
+// <copyright file="Strings.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiTools.Helpers.Check
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Check string for diffrent data.
+    /// </summary>
+    public static class Strings
+    {
+
+    }
+}
