132ef5ea65789b645165be55720cbc9f8f3036c0
Lennie Wennerlund <lennie@lempa.se>
2021-08-06 15:04:15 +0200

    documentation create update

--- BuildDoc - Convert - Release.bat
+++ BuildDoc - Convert - Release.bat
@@ -3,3 +3,6 @@ xmldocmd LiTools.Helpers.Convert\bin\Release\netstandard2.0\LiTools.Helpers.Conv
 xmldocmd LiTools.Helpers.Encoding\bin\Release\netstandard2.0\LiTools.Helpers.Encoding.dll Documentation --source https://github.com/LifeSocialLife/LiTools/tree/main/LiTools.Helpers.Encoding
 
 xmldocmd LiTools.Helpers.IO\bin\Release\netstandard2.0\LiTools.Helpers.IO.dll Documentation --source https://github.com/LifeSocialLife/LiTools/tree/main/LiTools.Helpers.IO
+
+xmldocmd LiTools.Helpers.Generate\bin\Release\netstandard2.0\LiTools.Helpers.Generate.dll Documentation --source https://github.com/LifeSocialLife/LiTools/tree/main/LiTools.Helpers.Generate
+
