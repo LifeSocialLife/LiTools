68b86ecb8aed86393d730e648a27f1332e28fa86
Lennie Wennerlund <lennie@lempa.se>
2022-02-02 23:06:03 +0100

    WIP

---
+++ LiTools.Helpers.DataAccess.MongoDb/Enums/QueryReturnStatusEnum.cs
@@ -0,0 +1,34 @@
+﻿// <summary>
+// MongoDb Query return status enum.
+// </summary>
+// <copyright file="QueryReturnStatusEnum.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiTools.Helpers.DataAccess.MongoDb.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Query return status.
+    /// </summary>
+    public enum QueryReturnStatusEnum
+    {
+        /// <summary>Qyery was corrct.</summary>
+        Ok,
+
+        /// <summary>Qyery has error.</summary>
+        Error,
+
+        /// <summary>Input data has error.</summary>
+        InputDataError,
+
+        /// <summary>Query has zero returns.</summary>
+        ZeroReturn,
+    }
+}
---
+++ LiTools.Helpers.DataAccess.MongoDb/Services/MongoDbService.cs
@@ -0,0 +1,53 @@
+﻿// <summary>
+// MongoDb Rundata service.
+// </summary>
+// <copyright file="MongoDbService.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+namespace LiTools.Helpers.DataAccess.MongoDb.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    public class MongoDbService
+    {
+        private string zzDebug { get; set; }
+
+        /// <summary>
+        /// Get size of model.
+        /// </summary>
+        /// <param name="data">the model to get size from.</param>
+        /// <returns>size as bytes.</returns>
+        public int GetModelSizeAsBsonAsBytes(object data)
+        {
+            int bytesSize = data.ToBson().Length;
+            return bytesSize;
+
+        }
+
+        /// <summary>
+        /// Can this model be saved to database.
+        /// </summary>
+        /// <param name="data">the model to check if it can be stored in mongo.</param>
+        /// <returns>true or false.</returns>
+        public bool ModelSizeFit(object data)
+        {
+            int size = this.GetModelSizeAsBsonAsBytes(data);
+
+            // 15,9 mb. Max size to store is 16mb.
+            if (size < 15900000)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
