4ee960be6611bfe4937a579ea4df0c33cfe49818
Lennie Wennerlund <lennie@lempa.se>
2022-02-21 15:46:51 +0100

    String checks added. contarins numbers, is lower or upp cast included. sql injections check. Mail adress check.

--- LiTools.Helpers.Check/Strings.cs
+++ LiTools.Helpers.Check/Strings.cs
@@ -10,8 +10,12 @@ namespace LiTools.Helpers.Check
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Linq;
     using System.Net;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     /// <summary>
     /// Check string for diffrent data.
@@ -55,6 +59,175 @@ namespace LiTools.Helpers.Check
             }
 
             return false;
+        }
+
+        #region Contains Number, Lower and Upper Casts.
+
+        /// <summary>
+        /// Contains numbers?.
+        /// </summary>
+        /// <param name="input">string.</param>
+        /// <returns>true or false.</returns>
+        public static bool ContainsNumbers(string input)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            return input.Any(char.IsDigit);
+        }
+
+        /// <summary>
+        /// Contains Lower casts?.
+        /// </summary>
+        /// <param name="input">string.</param>
+        /// <returns>true or false.</returns>
+        public static bool ContainsLowerCasts(string input)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            return input.Any(char.IsLower);
+        }
+
+        /// <summary>
+        /// Contains Upper casts?.
+        /// </summary>
+        /// <param name="input">string.</param>
+        /// <returns>true or false.</returns>
+        public static bool ContainsUpperCasts(string input)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            return input.Any(char.IsUpper);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// string contains sql injections code.
+        /// </summary>
+        /// <param name="input">string.</param>
+        /// <returns>true or false.</returns>
+        public static bool SqlSafe(string input)
+        {
+            if (input.Contains("'"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Check if email adress is a email adress.
+        /// </summary>
+        /// <param name="email">email adr as string.</param>
+        /// <returns>true or false.</returns>
+        public static bool IsMailValid(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+
+            email = email.Trim();
+
+            if (!IsMailValidCheck1(email))
+            {
+                return false;
+            }
+
+            if (!IsMailValidCheck2(email))
+            {
+                return false;
+            }
+
+            if (!IsMailValidCheck3(email))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsMailValidCheck1(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false; // suggested by @TK-421
+            }
+
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsMailValidCheck2(string email)
+        {
+            // System.ComponentModel.DataAnnotations;
+            // -
+            var foo = new EmailAddressAttribute();
+            return foo.IsValid(email);
+        }
+
+        private static bool IsMailValidCheck3(string email)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format
+            // -
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
         }
     }
 }
