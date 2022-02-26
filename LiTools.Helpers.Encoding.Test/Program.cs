f91d5506edf98e7c7fff117222cf67fab720d883
Lennie Wennerlund <lennie@lempa.se>
2022-02-26 15:35:03 +0100

    Test project added

---
+++ LiTools.Helpers.Encoding.Test/Program.cs
@@ -0,0 +1,10 @@
+using LiTools.Helpers.Encoding.Test;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
