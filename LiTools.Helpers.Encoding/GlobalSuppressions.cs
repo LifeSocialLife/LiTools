8c7a18cd4bdfc4589c3c8c3b1f8186a32236beaf
Lennie Wennerlund <lennie@lempa.se>
2022-03-02 13:55:32 +0100

    Global Suppressions

---
+++ LiTools.Helpers.Encoding/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// <summary>
+// Global Suppressions.
+// </summary>
+// <copyright file="GlobalSuppressions.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1123:Do not place regions within elements", Justification = "info.", Scope = "member", Target = "~M:LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.GenerateKeyPar(System.String,System.Int32)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "info.", Scope = "member", Target = "~M:LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.GenerateKey(System.String)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "info.", Scope = "member", Target = "~F:LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.zzDebug")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "info.", Scope = "member", Target = "~M:LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.GetKeysInStorage(LiTools.Helpers.Encoding.Cryptography.KeyTypesInStorage)~System.Collections.Generic.List{System.String}")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "info.", Scope = "member", Target = "~M:LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.GetKeysInStorage(LiTools.Helpers.Encoding.Cryptography.KeyTypesInStorage)~System.Collections.Generic.List{LiTools.Helpers.Encoding.Cryptography.RsaCryptoService.RsaCryptoService_GetKeysInStorageReturnModel}")]
