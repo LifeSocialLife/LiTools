c186a5c2318f2a7693e20c093d694808d7d5e5cd
Lennie Wennerlund <lennie@lempa.se>
2022-10-22 19:53:01 +0200

    function added

--- LiTools.Helpers.Convert/DateTimeConvert.cs
+++ LiTools.Helpers.Convert/DateTimeConvert.cs
@@ -10,6 +10,7 @@ namespace LiTools.Helpers.Convert
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Text;
 
     /// <summary>
@@ -35,6 +36,13 @@ namespace LiTools.Helpers.Convert
             }
         }
 
+        /// <summary>
+        /// String Y, M, D into datetime UTC.
+        /// </summary>
+        /// <param name="year">Y.</param>
+        /// <param name="mounth">M.</param>
+        /// <param name="day">D.</param>
+        /// <returns>bool Error, DateTime Date as UTC time.</returns>
         public static (bool Error, DateTime Date) StringToDateUtcFirstTimeOfDay(string year, string mounth, string day)
         {
             try
@@ -44,26 +52,119 @@ namespace LiTools.Helpers.Convert
             }
             catch
             {
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
                 return (true, DateTime.UtcNow);
             }
         }
 
-        //public static (bool Error, DateTime DateTime) StringToDateTimeUtc(string dt)
-        //{
-        //    try
-        //    {
-        //        DateTime tmpDate = Convert.ToDateTime("2000-01-01T00:00:00.000+00:00");
-        //        return (false, tmpDate);
-        //    }
-        //    catch
-        //    {
-        //        return (true, DateTime.UtcNow);
-        //    }
-        //}
+        /// <summary>
+        /// String date as 20220101 into datetime UTC.
+        /// </summary>
+        /// <param name="dateOnly">Y.</param>
+        /// <returns>bool Error, DateTime Date as UTC time.</returns>
+        public static (bool Error, DateTime Date) StringToDateUtcFirstTimeOfDay(string dateOnly)
+        {
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+            string zzDebug = "dsfdsf";
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+
+            if (string.IsNullOrEmpty(dateOnly))
+            {
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
+                return (true, GetDefaultDateAsUtc());
+            }
+
+            // Do we have - inside date string.
+            if (!dateOnly.Contains("-"))
+            {
+                // Date dont have - in date. ex 2022-10-22. i think the line is 20221022.. Rebuild syntax.
+                // lengt shod be 8 whitout -. check!.
+                if (dateOnly.Length != 8)
+                {
+                    // Lengt is not 8. it shod be 8.
+                    if (Debugger.IsAttached)
+                    {
+                        Debugger.Break();
+                    }
+
+                    return (true, GetDefaultDateAsUtc());
+                }
+
+#pragma warning disable CS0168 // Variable is declared but never used
+                try
+                {
+                    string year = dateOnly.Substring(0, 4);
+                    string mounth = dateOnly.Substring(4, 2);
+                    string day = dateOnly.Substring(6, 2);
+
+                    // dateOnly = $"{year}-{mounth}-{day}";
+                    return StringToDateUtcFirstTimeOfDay(year, mounth, day);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    // Error e.
+                    if (Debugger.IsAttached)
+                    {
+                        Debugger.Break();
+                    }
+
+                    return (true, GetDefaultDateAsUtc());
+                }
+#pragma warning restore CS0168 // Variable is declared but never used
+            }
+
+            // New code. check that code is working - 2022-10-22
+            zzDebug = "dfdf";
+
+            // We have - inside string. Check that is is 10 lengt. 4-2-2 + 2(-) ( 2010-01-01 )
+            if (dateOnly.Length != 10)
+            {
+                // Lengt is not 10. it shod be 8.
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
+                return (true, GetDefaultDateAsUtc());
+            }
+
+            var dd = dateOnly.Split('-');
+            if (dd.Length != 3)
+            {
+                // we shod have 3 in lengt. not this.
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break();
+                }
+
+                return (true, GetDefaultDateAsUtc());
+            }
+
+            zzDebug = "sdfdf";
+
+            return StringToDateUtcFirstTimeOfDay(dd[0], dd[1], dd[2]);
+        }
+
+        /// <summary>
+        /// Get datetime as UTC in 2000-01-01 00:00:00 UTC time.
+        /// </summary>
+        /// <returns>datetime 2000-01-01 00:00:00 UTC.</returns>
+        public static DateTime GetDefaultDateAsUtc()
+        {
+            return Convert.ToDateTime($"2000-01-01T00:00:00.000+00:00");
+        }
 
         /*
-         *             this.DtFirstDataExist = Convert.ToDateTime("2000-01-01T00:00:00.000+00:00"); // DateTime.UtcNow;
-            this.DtLastDataExist = Convert.ToDateTime("2000-01-01T00:00:00.000+00:00"); // DateTime.UtcNow;
+        this.DtFirstDataExist = Convert.ToDateTime("2000-01-01T00:00:00.000+00:00"); // DateTime.UtcNow;
+        this.DtLastDataExist = Convert.ToDateTime("2000-01-01T00:00:00.000+00:00"); // DateTime.UtcNow;
          * */
     }
 }
--- LiTools.Helpers.Convert/GlobalSuppressions.cs
+++ LiTools.Helpers.Convert/GlobalSuppressions.cs
@@ -10,3 +10,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements should appear in the correct order", Justification = "Reviewed.", Scope = "type", Target = "~T:LiTools.Helpers.Convert.DistanceEnums")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "Reviewed.", Scope = "type", Target = "~T:LiTools.Helpers.Convert.FileSizeEnums")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "Info.", Scope = "member", Target = "~M:LiTools.Helpers.Convert.DateTimeConvert.StringToDateUtcFirstTimeOfDay(System.String)~System.ValueTuple{System.Boolean,System.DateTime}")]
