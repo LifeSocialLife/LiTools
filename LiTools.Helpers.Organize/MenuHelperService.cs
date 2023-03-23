33fdbf1cd175bd6563e510af7f73770d71f8fcdf
Lennie Wennerlund <lennie@lempa.se>
2023-03-23 20:42:41 +0100

    nuget update

--- LiTools.Helpers.Organize/MenuHelperService.cs
+++ LiTools.Helpers.Organize/MenuHelperService.cs
@@ -13,7 +13,7 @@ namespace LiTools.Helpers.Organize
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-    using Microsoft.Extensions.Logging;
+    //using Microsoft.Extensions.Logging;
 
     /// <summary>
     /// Menu service.
