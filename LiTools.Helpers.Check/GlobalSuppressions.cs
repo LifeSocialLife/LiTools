28b3ca3eb40715bab7dbc5a950cdcf29108d5e22
Lennie Wennerlund <lennie@lempa.se>
2024-12-06 16:20:49 +0100

    wip

--- LiTools.Helpers.Check/GlobalSuppressions.cs
+++ LiTools.Helpers.Check/GlobalSuppressions.cs
@@ -7,5 +7,6 @@
 // <author>Lennie Wennerlund (lempa)</author>
 
 using System.Diagnostics.CodeAnalysis;
+
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "info.")]
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1311:Static readonly fields should begin with upper-case letter", Justification = "Info.", Scope = "member", Target = "~F:LiTools.Helpers.Check.Strings.sWhitespace")]
